d4c26347a15bbe4675c55c844f51c554d3e6f646
Rodrigo Borgognoni <rodrigo.borgognoni@jauserve.com.br>
2024-10-04 09:25:50 -0300

    video container shows video and desc

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+const containerVideos = document.querySelector('.videos__container');
+
 /* inicia o processo de busca de um recurso da rede, retornando uma promessa que é cumprida assim que a resposta estiver disponível. */
 const api = fetch('http://localhost:3000/videos')
-//.json() transforma a response em um json (array)
-.then((res) => console.log(res.json()));
+    //.json() transforma a response em um json
+    .then((res) => res.json())
+    .then((videos) => {
+        videos.forEach((video) => {
+            containerVideos.innerHTML += `<li class="videos__item">
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem} alt="logo canal"">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>
+            </div>
+        </li>`;
+        });
+    });
