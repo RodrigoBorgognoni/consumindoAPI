1d382595b5ec72a5367aa8621a2ec40239da17c5
Rodrigo Borgognoni <rodrigo.borgognoni@jauserve.com.br>
2024-10-07 14:19:54 -0300

    using try/catch

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const containerVideos = document.querySelector('.videos__container');
 
 async function searchVideos() {
-    /* inicia o processo de busca de um recurso da rede, retornando uma promessa que é cumprida assim que a resposta estiver disponível. */
+    try {
+        /* inicia o processo de busca de um recurso da rede, retornando uma promessa que é cumprida assim que a resposta estiver disponível. */
+        //await obriga a aguardar o retorno do fetch para continuar com a função assíncrona
+        const search = await fetch('http://localhost:3000/videos');
+        //.json() transforma a response em um json
+        const videos = await search.json();
 
-    //await obriga a aguardar o retorno do fetch para continuar com a função assíncrona
-    const search = await fetch('http://localhost:3000/videos');
-    //.json() transforma a response em um json
-    const videos = await search.json();
-
-    //para cada video que virá como resposta do JSON
-    //será passado esse video em específico como parâmetro na arrow function
-    videos.forEach((video) => {
-        containerVideos.innerHTML += `<li class="videos__item">
+        //para cada video que virá como resposta do JSON
+        //será passado esse video em específico como parâmetro na arrow function
+        videos.forEach((video) => {
+            containerVideos.innerHTML += `<li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
             <div class="descricao-video">
                 <img class="img-canal" src="${video.imagem} alt="logo canal"">
@@ -19,7 +19,11 @@ async function searchVideos() {
                 <p class="titulo-canal">${video.descricao}</p>
             </div>
         </li>` /* IFRAME incorpora outra página HTML para a pag atual */;
-    });
+        });
+    } catch (e) {
+        alert('ERROR 404: Video not found');
+        containerVideos.innerHTML = `${e}`
+    }
 }
 
 searchVideos();
