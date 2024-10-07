d93bff191f858fa3704ba6f3a0d568f95d1e5596
Rodrigo Borgognoni <rodrigo.borgognoni@jauserve.com.br>
2024-10-07 14:07:40 -0300

    using async/await

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const containerVideos = document.querySelector('.videos__container');
 
-/* inicia o processo de busca de um recurso da rede, retornando uma promessa que é cumprida assim que a resposta estiver disponível. */
-const api = fetch('http://localhost:3000/videos')
+async function searchVideos() {
+    /* inicia o processo de busca de um recurso da rede, retornando uma promessa que é cumprida assim que a resposta estiver disponível. */
+
+    //await obriga a aguardar o retorno do fetch para continuar com a função assíncrona
+    const search = await fetch('http://localhost:3000/videos');
     //.json() transforma a response em um json
-    .then((res) => res.json())
-    .then((videos) => {
-        //para cada video que virá como resposta do JSON
-        //será passado esse video em específico como parâmetro na arrow function
-        videos.forEach((video) => {
-            containerVideos.innerHTML += `<li class="videos__item">
+    const videos = await search.json();
+
+    //para cada video que virá como resposta do JSON
+    //será passado esse video em específico como parâmetro na arrow function
+    videos.forEach((video) => {
+        containerVideos.innerHTML += `<li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
             <div class="descricao-video">
                 <img class="img-canal" src="${video.imagem} alt="logo canal"">
@@ -16,9 +19,7 @@ const api = fetch('http://localhost:3000/videos')
                 <p class="titulo-canal">${video.descricao}</p>
             </div>
         </li>` /* IFRAME incorpora outra página HTML para a pag atual */;
-        });
-    })
-    .catch((error) => {
-        containerVideos.innerHTML = `
-    <p>Error ao Carregar videos: ${error}</p>`;
     });
+}
+
+searchVideos();
