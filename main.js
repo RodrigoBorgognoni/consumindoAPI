ab4a5d56ba6ce950d54dd088a1edbdd1c50d97bd
Rodrigo Borgognoni <rodrigo.borgognoni@jauserve.com.br>
2024-10-07 11:12:30 -0300

    iniciando tratamento de erro

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const api = fetch('http://localhost:3000/videos')
     //.json() transforma a response em um json
     .then((res) => res.json())
     .then((videos) => {
+        //para cada video que virá como resposta do JSON
+        //será passado esse video em específico como parâmetro na arrow function
         videos.forEach((video) => {
             containerVideos.innerHTML += `<li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -13,6 +15,10 @@ const api = fetch('http://localhost:3000/videos')
                 <h3 class="titulo-video">${video.titulo}</h3>
                 <p class="titulo-canal">${video.descricao}</p>
             </div>
-        </li>`;
+        </li>` /* IFRAME incorpora outra página HTML para a pag atual */;
         });
+    })
+    .catch((error) => {
+        containerVideos.innerHTML = `
+    <p>Error ao Carregar videos: ${error}</p>`;
     });
